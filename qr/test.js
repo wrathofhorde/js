775524028a8a65a55e15f39b01742fcbd1fc8291
wrath <wrathofhorde@gmail.com>
2024-05-31 11:01:43 +0900

    change qr

---
+++ qr/test.js
@@ -0,0 +1,22 @@
+document.write('<script type="text/javascript" src="qrcode.js"></script>');
+
+window.onload = () => {
+  const userId = "ydk";
+  const site = "@pravang.com";
+  const secret = "LERWRRNUJNONOUV7";
+  const content = `otpauth://totp/${userId}${site}?secret=${secret}`;
+  const qrcode = new QRCode(document.getElementById("qrcode"), {
+    width: 100,
+    height: 100,
+  });
+
+  const makeCode = (txt) => {
+    if (!txt) {
+      alert("input text");
+      return;
+    }
+    qrcode.makeCode(txt);
+  };
+
+  makeCode(content);
+};
