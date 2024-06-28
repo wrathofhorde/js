275d86bcc7e41808fb108c13ac85fdda9e893d68
wrath <wrathofhorde@gmail.com>
2024-06-28 14:47:44 +0900

    add qr test

--- qr/test.js
+++ qr/test.js
@@ -1,10 +1,10 @@
 document.write('<script type="text/javascript" src="qrcode.js"></script>');
 
 window.onload = () => {
-  const userId = "ydk";
-  const site = "@pravang.com";
+  const userId = "jason@pravang.com";
+  const site = "Pravang";
   const secret = "LERWRRNUJNONOUV7";
-  const content = `otpauth://totp/${userId}${site}?secret=${secret}`;
+  const content = `otpauth://totp/${userId} ${site}?secret=${secret}`;
   const qrcode = new QRCode(document.getElementById("qrcode"), {
     width: 100,
     height: 100,
