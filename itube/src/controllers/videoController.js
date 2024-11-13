4da904170dd2f0b97b37edc97489899a702ae8a9
wrath <wrathofhorde@gmail.com>
2024-11-13 17:20:45 +0900

    pug

--- itube/src/controllers/videoController.js
+++ itube/src/controllers/videoController.js
@@ -1,3 +1,22 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "ryan",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Raiders",
+    },
+    {
+      title: "Empire strikes back",
+    },
+    {
+      title: "Spider man",
+    },
+  ];
+
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const search = (req, res) => res.send("Search");
 export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
