fb5688da07de10596be1e69d3b8862941152a223
wrath <wrathofhorde@gmail.com>
2024-11-20 18:38:57 +0900

    add query video

--- itube/src/controllers/videoController.js
+++ itube/src/controllers/videoController.js
@@ -4,16 +4,23 @@ export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
     console.log(videos);
-    return res.render("home", { pageTitle: "Home", videos: [] });
+    return res.render("home", { pageTitle: "Home", videos });
   } catch (err) {
     console.log(err);
     return res.render("home", { pageTitle: "Home", videos: [] });
   }
 };
 
-export const watch = (req, res) => {
-  const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+export const watch = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+
+    return res.render("watch", { pageTitle: video.title, video });
+  } catch (err) {
+    console.log(err);
+    return res.redirect("/");
+  }
 };
 
 export const getEdit = (req, res) => {
@@ -32,8 +39,22 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `Upload Video` });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  try {
+    const { title, description, hashtags } = req.body;
+    const video = new Video({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
 
-  return res.redirect("/");
+    await video.save();
+    return res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    return res.render("upload", {
+      pageTitle: "Upload video",
+      errorMessage: err._message,
+    });
+  }
 };
--- itube/src/routers/videoRouter.js
+++ itube/src/routers/videoRouter.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { watch } from "../controllers/videoController";
+import {
+  getUpload,
+  postUpload,
+  watch,
+  getEdit,
+  postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
