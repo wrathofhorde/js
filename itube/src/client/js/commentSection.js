ff09b9f04404082a86de55e05033493b9646ebb9
wrath <wrathofhorde@gmail.com>
2025-01-06 18:49:58 +0900

    add comments

--- itube/src/client/js/commentSection.js
+++ itube/src/client/js/commentSection.js
@@ -1,5 +1,24 @@
 const form = document.getElementById("commentForm");
 
+const addComment = (text, id) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  console.log(videoComments);
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  newComment.setAttribute("data-id", id);
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  const markX = document.createElement("span");
+  markX.innerText = "❌";
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(markX);
+  console.log(newComment);
+  videoComments.prepend(newComment);
+};
+
 if (form) {
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -24,9 +43,15 @@ if (form) {
       }),
     });
 
-    if (response.status != 200) {
+    textarea.value = "";
+
+    if (response.status != 201) {
       console.log("fetch error");
       return;
     }
+
+    const { newCommentId } = await response.json();
+    // window.location.reload();
+    addComment(text, newCommentId);
   });
 }
--- itube/src/controllers/videoController.js
+++ itube/src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   try {
@@ -17,8 +18,10 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   try {
     const { id } = req.params;
-    const video = await Video.findById(id).populate("owner");
-
+    const video = await Video.findById(id)
+      .populate("owner")
+      .populate("comments");
+    console.log(video);
     if (!video) {
       return res.status(404).render("404", { pageTitle: "Video not found." });
     }
@@ -171,13 +174,26 @@ export const registerView = async (req, res) => {
 
 export const addComment = async (req, res) => {
   const {
-    params: id,
+    session: { user },
+    params: { id },
     body: { text },
   } = req;
 
-  console.log(req.body);
+  // console.log(id, text, user);
 
-  console.log(id, text);
+  const video = await Video.findById(id);
 
-  res.sendStatus(200);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    videos: id,
+  });
+
+  video.comments.push(comment._id);
+  video.save();
+  res.status(201).json({ newCommentId: comment._id });
 };
