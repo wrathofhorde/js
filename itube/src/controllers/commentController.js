e2a94da1085d5931c786cd47eb41360a522a2235
wrath <wrathofhorde@gmail.com>
2025-01-09 18:37:27 +0900

    check user id

--- itube/src/controllers/commentController.js
+++ itube/src/controllers/commentController.js
@@ -3,39 +3,44 @@ import Comment from "../models/Comment";
 import User from "../models/User";
 
 export const removeComment = async (req, res) => {
-  const {
-    session: { user },
-    body: { videoId, commentId },
-  } = req;
+  try {
+    const {
+      session: { user },
+      body: { videoId, commentId },
+    } = req;
 
-  const video = await Video.findById(videoId);
+    const video = await Video.findById(videoId);
+    console.log(video);
+    if (!video) {
+      return res.sendStatus(400);
+    }
 
-  if (!video) {
-    return res.sendStatus(400);
+    const commentUser = await User.findById(user._id);
+    console.log(commentUser);
+    if (!commentUser) {
+      return res.sendStatus(400);
+    }
+
+    const comment = await Comment.findById(commentId);
+    console.log(comment);
+    if (!comment) {
+      return res.sendStatus(400);
+    }
+
+    if (String(comment.owner) !== String(commentUser._id)) {
+      return res.sendStatus(401);
+    }
+
+    video.comments.pull(commentId);
+    await video.save();
+
+    commentUser.comments.pull(commentId);
+    await commentUser.save();
+
+    await Comment.deleteOne(comment);
+
+    return res.sendStatus(201);
+  } catch (e) {
+    console.log(e);
   }
-
-  const commentUser = await User.findById(user._id);
-  //   console.log(commentUser);
-  if (!commentUser) {
-    return res.sendStatus(400);
-  }
-
-  const comment = await Comment.findById(commentId);
-
-  if (!comment) {
-    return res.sendStatus(400);
-  }
-
-  //   console.log(video);
-  //   console.log(comment);
-
-  video.comments.pull(commentId);
-  await video.save();
-
-  commentUser.comments.pull(commentId);
-  await commentUser.save();
-
-  await Comment.deleteOne(comment);
-
-  return res.sendStatus(201);
 };
